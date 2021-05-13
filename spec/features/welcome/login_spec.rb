0cc211e6b4ae2468deb0426378e60a77d6b7245d
Wyattwicks <wyattwicks@gmail.com>
2021-05-13 16:11:11 -0600

    Implement Sign-in sad path

--- spec/features/welcome/login_spec.rb
+++ spec/features/welcome/login_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe "Login form (welcome page)" do
+  before :each do 
+    @user = User.create!(email: "123@test.com", password: "password123")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+  
   describe 'happy path' do
     it "can log in" do 
-      user = User.create!(email: "123@test.com", password: "password123")
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       visit root_path
       within(".login-form") do
         fill_in :email, with: "123@test.com"
@@ -22,9 +24,6 @@ RSpec.describe "Login form (welcome page)" do
 
   describe 'sad path' do
     it "can't log in without password" do 
-      user = User.create!(email: "123@test.com", password: "password123")
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       visit root_path
       within(".login-form") do
         fill_in :email, with: "123@test.com"
@@ -37,9 +36,6 @@ RSpec.describe "Login form (welcome page)" do
     end
 
     it "can't log in with incorrect password" do 
-      user = User.create!(email: "123@test.com", password: "password123")
-      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
-
       visit root_path
       within(".login-form") do
         fill_in :email, with: "123@test.com"
