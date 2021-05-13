e33b3e69b7d79778d5aa27dba557e94576668e04
Wyattwicks <wyattwicks@gmail.com>
2021-05-13 16:01:26 -0600

    Add sad path testing for log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,8 +2,13 @@ class SessionsController < ApplicationController
 
   def create
     user = User.find_by(email: params[:email])
-    session[:user_id] = user.id
-    flash[:info] = "Welcome, #{user.email}!"
-    redirect_to dashboard_path
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      flash[:info] = "Welcome, #{user.email}!"
+      redirect_to dashboard_path
+    else
+      redirect_to root_path
+      flash[:alert] = "Invalid Credentials"
+    end
   end
 end
--- spec/features/welcome/login_spec.rb
+++ spec/features/welcome/login_spec.rb
@@ -19,4 +19,36 @@ RSpec.describe "Login form (welcome page)" do
   
     end
   end
+
+  describe 'sad path' do
+    it "can't log in without password" do 
+      user = User.create!(email: "123@test.com", password: "password123")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+      within(".login-form") do
+        fill_in :email, with: "123@test.com"
+        fill_in :password, with: ""
+    
+        click_button "Sign In"
+      end
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Invalid Credentials")
+    end
+
+    it "can't log in with incorrect password" do 
+      user = User.create!(email: "123@test.com", password: "password123")
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
+
+      visit root_path
+      within(".login-form") do
+        fill_in :email, with: "123@test.com"
+        fill_in :password, with: "66666"
+    
+        click_button "Sign In"
+      end
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content("Invalid Credentials")
+    end
+  end
 end
