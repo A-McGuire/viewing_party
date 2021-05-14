4eba16ae5e3b049339c3cfc79f42318e53165bfe
Wyattwicks <wyattwicks@gmail.com>
2021-05-14 15:40:05 -0600

    Appease rubocop styling

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,4 @@
 class SessionsController < ApplicationController
-
   def create
     user = User.find_by(email: params[:email])
     if user && user.authenticate(params[:password])
@@ -8,7 +7,7 @@ class SessionsController < ApplicationController
       redirect_to dashboard_path
     else
       redirect_to root_path
-      flash[:alert] = "Invalid Credentials"
+      flash[:alert] = 'Invalid Credentials'
     end
   end
 end
