a7ad2fcc8ede46190ff2675f4f96c6d7492c7e66
Aidan McGuire <>
2021-05-15 09:30:15 -0600

    Refactor movies index to only use 1 ivar

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,6 +1,7 @@
 class MoviesController < ApplicationController
   def index
-    @movies1 = MovieService.top_rated_movies(1)
-    @movies2 = MovieService.top_rated_movies(2)
+    movies1 = MovieService.top_rated_movies(1)
+    movies2 = MovieService.top_rated_movies(2)
+    @movies = movies1[:results].concat(movies2[:results])
   end
 end
