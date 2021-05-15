aa0014df5253b70d6eea4691163ba02daabf0973
Aidan McGuire <>
2021-05-15 17:04:17 -0600

    Add test for discover movies button on dashaboard

--- spec/features/users/index_spec.rb
+++ spec/features/users/index_spec.rb
@@ -1,9 +1,12 @@
 require 'rails_helper'
 
-RSpec.describe 'User Dashboard' do 
+RSpec.describe 'User Dashboard' do \
+  before :each do |test|
+    @user = User.create!(email: "123@email.com", password: "1111")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user) unless test.metadata[:logged_out]
+  end
+
   it "I see my email displayed in a welcome message" do 
-    user = User.create!(email: "123@email.com", password: "1111")
-    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(user)
 
     visit dashboard_path
     
@@ -12,10 +15,27 @@ RSpec.describe 'User Dashboard' do
     expect(page).to have_content("Welcome, 123@email.com")
   end
   
-  it "redirects unauthenticated users to root" do
+  it "redirects unauthenticated users to root", :logged_out do
     visit '/dashboard'
     
     expect(current_path).to eq(root_path)
     expect(page).to have_content("Please login")
   end
+  
+  it 'has a button to discover movies' do
+    visit '/dashboard'
+
+    expect(page).to have_button("Discover Movies")
+    click_button "Discover Movies"
+    expect(current_path).to eq("/discover")
+  end
+
+  describe 'friends section' do
+
+  end
+  
+  describe 'viewing party section' do
+
+  end
+
 end
