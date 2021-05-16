d82d54f4bbba44a0e9ea9ea3b81753f886596e3e
Aidan McGuire <>
2021-05-16 13:03:27 -0600

    Make movies index DRY

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   def index
     if params[:movie_title].present?
-      @title_search = Movies.title_search_results(params[:movie_title])
+      @movies = Movies.title_search_results(params[:movie_title])
     elsif params[:display] == 'top40'
       @movies = Movies.top40
     end
