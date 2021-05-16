56dfb92dc5f339b104644d6b6802be03dd0c0689
Aidan McGuire <>
2021-05-16 13:02:08 -0600

    Refactor movies controller, only call api when data is to be displayed

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,10 @@
 class MoviesController < ApplicationController
   def index
-    @movies = Movies.top40
-    return unless params[:movie_title].present?
-
-    @title_search = Movies.title_search_results(params[:movie_title])
+    if params[:movie_title].present?
+      @title_search = Movies.title_search_results(params[:movie_title])
+    elsif params[:display] == 'top40'
+      @movies = Movies.top40
+    end
   end
 
   def discover; end
