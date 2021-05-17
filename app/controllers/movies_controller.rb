5cc4cdcf7f54c23f3f6a032014233b7b11bb9899
Wyattwicks <wyattwicks@gmail.com>
2021-05-17 14:29:48 -0600

    Refactor API to include facade

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,9 +1,9 @@
 class MoviesController < ApplicationController
   def index
     if params[:movie_title].present?
-      @movies = Movies.title_search_results(params[:movie_title])
+      @movies = MovieFacade.title_search_results(params[:movie_title])
     elsif params[:display] == 'top40'
-      @movies = Movies.top40
+      @movies = MovieFacade.top40
     end
   end
 
