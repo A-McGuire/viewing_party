fd3304643c2d881109151f1d9ec9ff4fd6dfac24
Wyattwicks <wyattwicks@gmail.com>
2021-05-17 18:13:04 -0600

    Add log out button

--- config/routes.rb
+++ config/routes.rb
@@ -10,6 +10,8 @@ Rails.application.routes.draw do
 
   post "/login", to: "sessions#create"
 
+  get "/logout", to: "sessions#destroy"
+
   #restful
   resources :users, only: [:create]
   
---
+++ spec/features/discover/index_spec.rb
@@ -0,0 +1,41 @@
+require 'rails_helper'
+
+RSpec.describe "Discover page" do
+  before :each do |test|
+    @user = User.create!(email: "123@email.com", password: "1111")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user) unless test.metadata[:logged_out]
+  end
+
+  it "redirects unauthenticated users to root", :logged_out do
+    visit discover_path
+    
+    expect(current_path).to eq(root_path)
+    expect(page).to have_content("Please login")
+  end
+
+  it 'has a link to top 40 movies (/movies)', :vcr do
+    visit discover_path
+
+    expect(page).to have_button('Find Top Rated Movies')
+    click_button 'Find Top Rated Movies'
+    expect(current_path).to eq(movies_path)
+  end
+
+  it 'has a form to search movies by title', :vcr do
+    visit discover_path
+
+    fill_in :movie_title, with: "Fight Club"
+    expect(page).to have_button('Find Movies')
+    click_button 'Find Movies'
+    expect(current_path).to eq(movies_path)
+    expect(page).to have_content("Fight Club")
+    expect(page).to have_content("Florence Fight Club")
+  end
+
+  it "has a link back to dashboard" do 
+    visit discover_path
+    expect(page).to have_link("Dashboard")
+    click_link("Dashboard")
+    expect(current_path).to eq(dashboard_path)
+  end
+end
