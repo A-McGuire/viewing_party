f7b1b75363120a4bbd203ac5b30e75158135a9d1
Wyattwicks <wyattwicks@gmail.com>
2021-05-18 19:33:55 -0600

    Add table friendships with relationships

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -6,4 +6,8 @@ RSpec.describe User do
     it {should validate_uniqueness_of :email}
     it {should validate_presence_of :password_digest}
   end 
+  describe 'relationships' do 
+    it {should have_many :friendships}
+    it {should have_many(:friends).through(:friendships)}
+  end 
 end 
